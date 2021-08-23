0c2b6f1f47837f41cc20631ddf1e24e5b44f4700
Your Name <you@example.com>
2021-08-23 15:02:36 -0500

    20210823

--- assignments/01Promedio/tests/test_exercise.py
+++ assignments/01Promedio/tests/test_exercise.py
@@ -41,3 +41,15 @@ def test_exercise(value, result, message):
     src.exercise.main()
 
     assert output == result, message
+
+def main():
+    #escribe tu código abajo 
+    c1=int(input("dame calif 1"))
+    c2=int(input("dame calif 2"))
+    c3=int(input("dame calif 3"))
+    c4=int(input("dame calif 4"))
+    promedio= float((c1+c2+c3+c4)/4)
+    print("el promedio es" + str(promedio))
+
+if __name__ == '__main__':
+    main()
